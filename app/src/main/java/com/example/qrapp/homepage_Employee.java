d5579835df203a2b2283233b09e4a56f43b51041
Vudung201 <1911505310109@sv.ute.udn.vn>
2021-12-19 17:54:57 +0700

    vudung

---
+++ app/src/main/java/com/example/qrapp/homepage_Employee.java
@@ -0,0 +1,13 @@
+package com.example.qrapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class homepage_Employee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage_employee);
+    }
+}
---
+++ app/src/main/java/com/example/qrapp/login_Activity.java
@@ -0,0 +1,19 @@
+package com.example.qrapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
+        setContentView(R.layout.activity_login);
+    }
+}
