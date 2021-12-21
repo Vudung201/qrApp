d9d1719d317889767ce2f2ccde4674514eea3477
Vudung201 <1911505310109@sv.ute.udn.vn>
2021-12-21 16:08:55 +0700

    vudung

--- app/src/main/java/com/example/qrapp/homePage.java
+++ app/src/main/java/com/example/qrapp/homePage.java
@@ -2,12 +2,29 @@ package com.example.qrapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class homePage extends AppCompatActivity {
+    Button scanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_home_page);
+        scanButton = findViewById(R.id.button_ScanQR);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
